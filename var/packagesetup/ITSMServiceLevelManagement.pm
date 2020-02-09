a25fe3368d1be25d686549476ebfaeda15489fda
StefanRother-OTOBO <sr@otobo.ch>
2020-02-09 06:31:14 +0000

    Initial OTOBO release, yippie :) !

---
+++ var/packagesetup/ITSMServiceLevelManagement.pm
@@ -0,0 +1,164 @@
+# --
+# OTOBO is a web-based ticketing system for service organisations.
+# --
+# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+# Copyright (C) 2019-2020 Rother OSS GmbH, https://otobo.de/
+# --
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# --
+
+package var::packagesetup::ITSMServiceLevelManagement;    ## no critic
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::System::Stats',
+);
+
+=head1 NAME
+
+var::packagesetup::ITSMServiceLevelManagement - code to execute during package installation
+
+=head1 PUBLIC INTERFACE
+
+=cut
+
+=head2 new()
+
+create an object
+
+    use Kernel::System::ObjectManager;
+    local $Kernel::OM = Kernel::System::ObjectManager->new();
+    my $CodeObject = $Kernel::OM->Get('var::packagesetup::ITSMServiceLevelManagement');
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # Force a reload of ZZZAuto.pm and ZZZAAuto.pm to get the fresh configuration values.
+    for my $Module ( sort keys %INC ) {
+        if ( $Module =~ m/ZZZAA?uto\.pm$/ ) {
+            delete $INC{$Module};
+        }
+    }
+
+    # Create common objects with fresh default config.
+    $Kernel::OM->ObjectsDiscard();
+
+    # define UserID parameter for the constructor of the stats object
+    $Kernel::OM->ObjectParamAdd(
+        'Kernel::System::Stats' => {
+            UserID => 1,
+        },
+    );
+
+    # define file prefix
+    $Self->{FilePrefix} = 'ITSMStats';
+
+    return $Self;
+}
+
+=head2 CodeInstall()
+
+run the code install part
+
+    my $Result = $CodeObject->CodeInstall();
+
+=cut
+
+sub CodeInstall {
+    my ( $Self, %Param ) = @_;
+
+    # install stats
+    $Kernel::OM->Get('Kernel::System::Stats')->StatsInstall(
+        FilePrefix => $Self->{FilePrefix},
+        UserID     => 1,
+    );
+
+    return 1;
+}
+
+=head2 CodeReinstall()
+
+run the code reinstall part
+
+    my $Result = $CodeObject->CodeReinstall();
+
+=cut
+
+sub CodeReinstall {
+    my ( $Self, %Param ) = @_;
+
+    # install stats
+    $Kernel::OM->Get('Kernel::System::Stats')->StatsInstall(
+        FilePrefix => $Self->{FilePrefix},
+        UserID     => 1,
+    );
+
+    return 1;
+}
+
+=head2 CodeUpgrade()
+
+run the code upgrade part
+
+    my $Result = $CodeObject->CodeUpgrade();
+
+=cut
+
+sub CodeUpgrade {
+    my ( $Self, %Param ) = @_;
+
+    # install stats
+    $Kernel::OM->Get('Kernel::System::Stats')->StatsInstall(
+        FilePrefix => $Self->{FilePrefix},
+        UserID     => 1,
+    );
+
+    return 1;
+}
+
+=head2 CodeUninstall()
+
+run the code uninstall part
+
+    my $Result = $CodeObject->CodeUninstall();
+
+=cut
+
+sub CodeUninstall {
+    my ( $Self, %Param ) = @_;
+
+    # uninstall stats
+    $Kernel::OM->Get('Kernel::System::Stats')->StatsUninstall(
+        FilePrefix => $Self->{FilePrefix},
+        UserID     => 1,
+    );
+
+    return 1;
+}
+
+1;
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTOBO project (L<https://otobo.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see L<https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+=cut
